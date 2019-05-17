e95575aa9b34b68173d144def8cddebc3d641c78
Zachary Kwiecinski <Zachary.Kwiecinski@gmail.com>
2019-05-17 17:36:28 +0200

    create defulat image

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,15 @@ ingredients['drinks'].each do |ingredient|
   Ingredient.create!(name: ingredient['strIngredient1'])
 end
 
-10.times do
-  name = Faker::Name.unique.first_name
-  cocktail = Cocktail.new(name: name)
-  cocktail.remote_photo_url = "https://picsum.photos/200/300"
-  cocktail.save!
-  rand(6..10).times do
-    dose = Dose.new(description: Faker::Science.element)
-    dose.cocktail = cocktail
-    dose.ingredient = Ingredient.all.sample
-    dose.save!
-  end
-end
+# 10.times do
+#   name = Faker::Name.unique.first_name
+#   cocktail = Cocktail.new(name: name)
+#   cocktail.remote_photo_url = "https://picsum.photos/200/300"
+#   cocktail.save!
+#   rand(6..10).times do
+#     dose = Dose.new(description: Faker::Science.element)
+#     dose.cocktail = cocktail
+#     dose.ingredient = Ingredient.all.sample
+#     dose.save!
+#   end
+# end
